aa9eccd3d11d4cd6bb8470fcd99265d5e790d35e
Bonventure2010 <166444173+Bonventure2010@users.noreply.github.com>
2024-04-09 14:51:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'ice', true],
+  ['917605902011', 'ice2', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
